04359a213c5a5c0042aa4cb064167cd078ce0625
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-03-27 11:19:05 -0700

    add artifact bucket to outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "clone_repo_https" {
 output "clone_repo_ssh" {
   value = "${aws_codecommit_repository.repo.clone_url_ssh}"
 }
+
+output "artifact_bucket" {
+  value = "${aws_s3_bucket.build_artifact_bucket.id}"
+}
