8e2a266caa4bcb231191c4f893fe30fab1027dae
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-03-27 11:27:26 -0700

    bubble outputs back out for basic-example

---
+++ examples/basic-example/outputs.tf
@@ -0,0 +1,11 @@
+output "clone_repo_https" {
+  value = "${module.basic_example.clone_repo_https}"
+}
+
+output "clone_repo_ssh" {
+  value = "${module.basic_example.clone_repo_ssh}"
+}
+
+output "artifact_bucket" {
+  value = "${module.basic_example.artifact_bucket}"
+}
