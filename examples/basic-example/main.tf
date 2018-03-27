8b7af50c3e5076f7a47e75845e1b5f248e8e2ee8
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-03-27 10:56:41 -0700

    update basic-example to pass true for forcing the destruction of the artifact bucket

--- examples/basic-example/main.tf
+++ examples/basic-example/main.tf
@@ -3,16 +3,17 @@ provider "aws" {
 }
 
 module "basic_example" {
-  source              = "git::https://github.com/slalompdx/terraform-aws-codecommit-cicd.git?ref=master"
-  repo_name           = "slalom-devops"
-  organization_name   = "slalom"
-  repo_default_branch = "master"
-  aws_region          = "us-west-2"
-  char_delimiter      = "-"
-  environment         = "dev"
-  build_timeout       = "5"
-  build_compute_type  = "BUILD_GENERAL1_SMALL"
-  build_image         = "aws/codebuild/nodejs:6.3.1"
-  test_buildspec      = "buildspec_test.yml"
-  package_buildspec   = "buildspec.yml"
+  source                 = "git::https://github.com/slalompdx/terraform-aws-codecommit-cicd.git?ref=master"
+  repo_name              = "slalom-devops"
+  organization_name      = "slalom"
+  repo_default_branch    = "master"
+  aws_region             = "us-west-2"
+  char_delimiter         = "-"
+  environment            = "dev"
+  build_timeout          = "5"
+  build_compute_type     = "BUILD_GENERAL1_SMALL"
+  build_image            = "aws/codebuild/nodejs:6.3.1"
+  test_buildspec         = "buildspec_test.yml"
+  package_buildspec      = "buildspec.yml"
+  force_artifact_destroy = true
 }
