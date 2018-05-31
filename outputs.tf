16f863d38b916c64065b18e35072a1dcfce76f6c
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-05-30 21:19:10 -0700

    include names

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,14 @@ output "codepipeline_role" {
   value = "${aws_iam_role.codepipeline_role.arn}"
 }
 
+output "codepipeline_role_name" {
+  value = "${aws_iam_role.codepipeline_role.id}"
+}
+
 output "codebuild_role" {
   value = "${aws_iam_role.codebuild_assume_role.arn}"
+}
+
+output "codebuild_role_name" {
+  value = "${aws_iam_role.codebuild_assume_role.id}"
 }
