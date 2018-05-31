45db9aff4694707c11e53e29170244132fe3ef47
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-05-30 21:02:51 -0700

    output role arns

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "clone_repo_ssh" {
 output "artifact_bucket" {
   value = "${aws_s3_bucket.build_artifact_bucket.id}"
 }
+
+output "codepipeline_role" {
+  value = "${aws_iam_role.codepipeline_role.arn}"
+}
+
+output "codebuild_role" {
+  value = "${aws_iam_role.codebuild_assume_role.arn}"
+}
