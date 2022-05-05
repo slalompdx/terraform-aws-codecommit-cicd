264aa069f0cd655f73f32d3d091395bb98ac9daa
Federico Nafria <federiconafria@gmail.com>
2022-05-05 08:21:45 +0200

    Remove deprecated acl parameter

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "aws_codecommit_repository" "repo" {
 # CodePipeline resources
 resource "aws_s3_bucket" "build_artifact_bucket" {
   bucket        = module.unique_label.id
-  acl           = "private"
   force_destroy = var.force_artifact_destroy
 }
 
